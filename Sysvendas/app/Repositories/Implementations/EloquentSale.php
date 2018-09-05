4e6cbf44c43befb8772cfc34a138877700610e98
João Paulo Gonçalves Queiroz <joao.queiroz@causp.sede>
2018-09-05 11:18:18 -0300

    Crete class to respository sale

---
+++ Sysvendas/app/Repositories/Implementations/EloquentSale.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Repositories\Implementations;
+
+use App\Repositories\Base\EloquentRepository;
+use App\Repositories\Interfaces\SaleInterface;
+use App\Models\Sale;
+
+/**
+ * Class de implemtação do repositorio de sale
+ */
+class EloquentSale extends EloquentRepository implements SaleInterface
+{
+    public function __construct(Sale $sale)
+    {
+        $this->model = $sale;
+    }
+}
